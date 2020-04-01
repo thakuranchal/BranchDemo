5b11d9e8c0903f643c58a195ca3a3bb763b6199e
thakuranchal <62846070+thakuranchal@users.noreply.github.com>
2020-04-01 08:56:02 +0530

    move the file

---
+++ AcessModifiers.java
@@ -0,0 +1,11 @@
+package AccessModifierPkg2;
+
+//import AccessModifiersPkg1.Protected;
+public class AcessModifiers {
+	private int a=90;
+	int b=30;
+	public int c=20;
+	protected int d=10;
+	
+
+}
---
+++ MainClass.java
@@ -0,0 +1,9 @@
+package AccessModifierPkg2;
+
+public class MainClass {
+	public static void main(String[] args) {
+		AcessModifiers obj = new AcessModifiers();
+		System.out.println(obj.b +"-"+obj.c+"-"+obj.d);
+	}
+
+}
